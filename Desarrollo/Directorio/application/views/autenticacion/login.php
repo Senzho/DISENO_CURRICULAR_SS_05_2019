4c6c9664e7765e6234ae390115422ddb25ef721c
Senzho <vijagama@outlook.es>
2019-11-27 02:31:36 -0600

    CSS login
    Se añade CSS a la página de Login.

--- Desarrollo/Directorio/application/views/autenticacion/login.php
+++ Desarrollo/Directorio/application/views/autenticacion/login.php
@@ -4,16 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
 <head>
+    <link href="<?php echo base_url();?>css/estilo.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url();?>css/login.css" rel="stylesheet" type="text/css">
 </head>
-<body>
-    <div>
-        <h1>Iniciar Sesión</h1>
-        <?php echo form_open('Autenticacion/iniciar_sesion',array('id'=>'', 'class'=>''))?>
-            <input type="text" id="" class="" placeholder="Usuario" required="" autofocus="" name="nick">
-            <input type="password" id="" class="" placeholder="Contraseña" required="" name="contraseña">
-            <button class="" type="submit" id="">Ingresar</button>
-        </form>
-        <a href="">¿Olvidaste tu contraseña?</a>
+<body class="cuerpo">
+    <div id="pagina" class="pagina centradoVerticalPadre">
+        <div id="bloqueLogin" class="bloque centradoHorizontal centradoVerticalPadre">
+            <div id="bloqueFormulario" class="centradoHorizontal">
+                <h1 class="titulo1 centro">Iniciar sesión</h1>
+                <?php echo form_open('Autenticacion/iniciar_sesion',array('id'=>'', 'class'=>''))?>
+                    <input type="text" id="" class="control campoTexto block centradoHorizontal" placeholder="Usuario" required="" autofocus="" name="nick">
+                    <input type="password" id="" class="control campoTexto block centradoHorizontal" placeholder="Contraseña" required="" name="contraseña">
+                    <input class="boton block centradoHorizontal" type="submit" id="" value="Ingresar"/>
+                </form>
+                <a href="" class="block centro">¿Olvidaste tu contraseña?</a>
+            </div>
+        </div>
     </div>
     <div>
         <label><?php echo $mensaje;?></label>
